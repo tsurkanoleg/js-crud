feba3c71e168b0881bc5ccb508363ef83011010b
tsurkanoleg <tsurkanoleg1993@gmail.com>
2023-08-11 15:15:08 +0300

    +

--- src/route/index.js
+++ src/route/index.js
@@ -9,53 +9,56 @@ class Product {
   static #list = []; 
 
   constructor(name, price, description,) {   
-    this.id = this.Math.floor(Math.random() * 90000) + 10000;
-    this.createDate = new Date().toISOString();
+	this.id = Math.floor(Math.random() * 100000);
+	this.createDate = () => {
+		this.date = new Date().toISOString()
+	  }
     this.name = name;
     this.price = price;
     this.description = description;
   }
   
-  getList() {
-    return this.#list;
-  }
+  static getList = () => this.#list;
+  checkId = (id) => this.id === id
 
-  add(product) {
+  static add = (product) => {
     this.#list.push(product);
   }
 
-  getById(id) {
-    const numericId = parseInt(id, 10);
-    if (!isNaN(numericId)) {
-      return this.#list.find(product => product.id === numericId);
-    }
-    return null;
-  }
+  static getById = (id) =>
+  this.#list.find((product) => product.id === id)
 
-  updateById(id, data) {
+  // static updateById = (id, data) => {
+  // const product = this.getById(id)
+  // }
+
+	static deleteById = (id) => {
+    const index = this.#list.findIndex(
+      (product) => product.id === id,
+    )
+    if (index !== -1) {
+      this.#list.splice(index, 1)
+      return true
+    } else {
+      return false
+    }
+	}
+
+  static updateById(id, data) {
     const product = this.getById(id);
     if (product) {
       if (data.name) product.name = data.name;
       if (data.price) product.price = data.price;
       if (data.description) product.description = data.description;
       return product;
-    }
-    return null;
-  }
-
-  deleteById(id) {
-    const index = this.#list.findIndex(product => product.id === id);
-    if (index !== -1) {
-      this.#list.splice(index, 1);
-      return true;
-    }else {      
+    } else {
       return false;
     }
+        
   }
 
-  // static create = (product) => {
-  //   this.#list.push(product)
-  // }
+
+
 
 }
 
@@ -87,7 +90,7 @@ router.get('/', function (req, res) {
 
 // ↙️ тут вводимо шлях (PATH) до сторінки
 router.get('/product-create', function (req, res) {
-  console.log(req.body)
+  const list = Product.getList()
   res.render('product-create', {
     
     style: 'product-create',
@@ -96,24 +99,34 @@ router.get('/product-create', function (req, res) {
 
 // ================================================================
 
-// ↙️ тут вводимо шлях (PATH) до сторінки
 router.post('/product-create', function (req, res) {
-  // const {name, price, description} = req.body;
-  // const newProduct = new Product(name, price, description);
+  const {name, price, description} = req.body;
 
-  res.render('product-create', {
-    style: 'product-create',
+  const product = new Product(name, price, description);
 
+  Product.add(product)
 
+  console.log(Product.getList)
+
+  res.render('product-alert', {
+    style: 'product-alert',
+    info:"Товар успішно доданий"
   })
-  // ↑↑ сюди вводимо JSON дані
 })
 
 // ================================================================
 
 router.get('/product-list', function (req, res) {
+  const list = Product.getList()
+  console.log(list)
   res.render('product-list', {
     style: 'product-list',
+    data:{
+      product:{
+        list,
+        isEmpty: list.length === 0
+      }
+    }
   })
 })
 
@@ -128,30 +141,74 @@ router.get('/product-alert', function (req, res) {
 // ================================================================
 
 router.get('/product-edit', function (req, res) {
+  const { id } = req.query
+  const product = Product.getById(Number(id))
 	
-	res.render('product-edit', {		
-	  style: 'product-edit',		
-	})
+  if (product) {
+    return res.render('product-edit', {
+        style: 'product-edit',
+        data: {
+          name: product.name,
+          price: product.price,
+          id: product.id,
+          description: product.description,
+        },
+      })
+    } else {
+      return res.render('product-alert', {
+        style: 'product-alert',
+        info: 'Продукту за таким ID не знайдено',
+      })
+    }
+
   })
   
 // ================================================================
   
 router.post('/product-edit', function (req, res) {
-	
-	res.render('product-edit', {		
-	  style: 'product-edit',		
-	})
+  const { id, name, price, description } = req.body
+  const product = Product.updateById(Number(id), {
+    name,
+    price,
+    description,
   })
-  
+  console.log(id)
+  console.log(product)
+
+  if (product) {
+    
+    res.render('product-edit', {
+      style: 'product-edit',
+      info: 'Інформація про товар оновлена',
+    })
+  } else {
+    res.render('product-edit', {
+      style: 'product-edit',
+      info: 'Сталася помилка',
+    })
+  }
+
+  })
+
 // ================================================================
+
+router.get('/product-delete', function (req, res) {
   
-router.get('/product-edit', function (req, res) {
-	
-	res.render('product-edit', {		
-	  style: 'product-edit',		
-	})
+  const { id } = req.query
+  Product.deleteById(Number(id))
+  res.render('product-alert', {
+    style: 'product-alert',
+    info: 'Товар видалений',
   })
-  
+})
+
+// ================================================================
+
+
+
+// ================================================================
+
+
 // ================================================================
 
 
